2f31a1ff52c0493332b6c169937449f031e6bb07
ganebe <Lok>
2023-10-14 13:25:35 -0700

    v1

---
+++ bintree.h
@@ -0,0 +1,36 @@
+#ifndef BINTREE_H
+#define BINTREE_H
+#include <iostream>
+#include "nodedata.h"
+using namespace std;
+
+
+class BinTree{
+
+public:
+	BinTree();								// constructor
+	BinTree(const BinTree &);				// copy constructor
+	~BinTree();								// destructor, calls makeEmpty	
+	bool isEmpty() const;					// true if tree is empty, otherwise false
+	void makeEmpty();						// make the tree empty so isEmpty returns true
+	BinTree& operator=(const BinTree &);
+	bool operator==(const BinTree &) const;
+	bool operator!=(const BinTree &) const;
+	bool insert(NodeData*);
+	bool retrieve(...) const;
+	void displaySideways() const;			// provided below, displays the tree sideways
+
+private:
+	struct Node {
+		NodeData* data;						// pointer to data object
+		Node* left;							// left subtree pointer
+		Node* right;						// right subtree pointer
+	};
+	Node* root;								// root of the tree
+
+// utility functions
+    void inorderHelper( ... ) const;
+    void sideways(Node*, int) const;			// provided below, helper for displaySideways()
+};
+
+#endif
