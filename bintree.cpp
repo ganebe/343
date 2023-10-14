29c484376e9f34155c1421f71f21f6f60f759ca8
ganebe <Lok>
2023-10-14 14:08:44 -0700

    v3

--- bintree.cpp
+++ bintree.cpp
@@ -5,7 +5,7 @@
 // hard coded displaying to standard output.
 // Preconditions: NONE
 // Postconditions: BinTree remains unchanged.
-void BinTree::displaySideways() const {
+void BinTree::displaySideways() const{
 	sideways(root, 0);
 }
 
@@ -13,17 +13,25 @@ void BinTree::displaySideways() const {
 // Helper method for displaySideways
 // Preconditions: NONE
 // Postconditions: BinTree remains unchanged.
-void BinTree::sideways(Node* current, int level) const {
-	if (current != NULL) {
+void BinTree::sideways(Node* current, int level) const{
+	if (current != NULL){
 		level++;
 		sideways(current->right, level);
 
 		// indent for readability, 4 spaces per depth level 
-		for (int i = level; i >= 0; i--) {
+		for (int i = level; i >= 0; i--){
 			cout << "    ";
 		}
 
 		cout << *current->data << endl;        // display information of object
 		sideways(current->left, level);
 	}
+}
+
+bool BinTree::isEmpty() const{
+    if(root == NULL){
+        return true;
+    }else{
+        return false;
+    }
 }
