78e9d2cd7c779bb8ce9ce5a16edd9bb4398c3335
ganebe <Lok>
2023-10-15 21:09:56 -0700

    getHeight

--- bintree.h
+++ bintree.h
@@ -40,6 +40,8 @@ private:
 	void makeEmpty();
 	void makeEmptyHelper(Node*&);
 	void nodeCopyHelper(Node* &,const Node* &);
+	bool exist(const Node* &, const NodeData &, Node*&) const;
+	int  getHeightHelper(Node* &)const;
 };
 
 #endif
