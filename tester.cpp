8c0af44fa48afabb51e1a10d248d3fde4f2da9ce
ganebe <Lok>
2023-10-20 21:49:44 -0700

    added tester for extra test cases

---
+++ tester.cpp
@@ -0,0 +1,57 @@
+#include "nodedata.h"
+#include "bintree.h"
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+void initArray(NodeData*[]);             
+
+const int ARRAYSIZE = 100;
+
+
+int main(){
+    NodeData ndA("a");
+    NodeData ndAB("ab");
+    NodeData ndAC("ac");
+    BinTree  A;
+    A.insert(&ndA);
+    A.insert(&ndAB);
+    A.insert(&ndAC);
+    A.displaySideways();
+    cout << A << endl;
+    NodeData* ndArray[ARRAYSIZE];
+    initArray(ndArray);
+    A.bstreeToArray(ndArray);
+    A.arrayToBSTree(ndArray);
+    A.displaySideways();
+    cout << A << endl;
+    cout << "after array to bstree and bstree to array" << endl;
+    cout << "chagne A to B, ab to bb ac to cc" << endl;
+    ndA = ndAB;
+    ndAB = ndAB;
+    ndAC = ndAB;
+    A.displaySideways();
+    cout << A << endl; 
+    cout << ndA << ndAB << ndAC;
+    BinTree C(A);
+    NodeData* nde;
+    nde = new NodeData("e");
+    cout << "comparing c and a" <<endl;
+    C.insert(nde);
+    A.displaySideways();
+    cout << A << endl; 
+    C.displaySideways();
+    cout << C << endl;
+
+    delete nde;
+    cout << "after deleting nde pointer" << endl;
+    C.displaySideways();
+    cout << C << endl;
+    cout << "after deleting nde pointer" << endl;
+}
+
+void initArray(NodeData* ndArray[]) {
+	for (int i = 0; i < ARRAYSIZE; i++)
+		ndArray[i] = NULL;
+}
